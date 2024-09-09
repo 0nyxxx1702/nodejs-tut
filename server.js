52b1554b77de8096de53ef428acb268e9319e41c
Swastik Ranjan Panigrahi <swastikrpanigrahi@gmail.com>
2024-09-09 12:29:30 +0530

    middlewares

--- server.js
+++ server.js
@@ -1,8 +1,33 @@
 const express=require('express')
 const app=express();
+const cors=require('cors');
+const {logger}=require('./middlewares/logEvents');
+const errorHandler=require('./middlewares/ErrorHandler');
 const path = require('path');
 
 const PORT = process.env.PORT || 3500;
+
+app.use(logger);
+
+const whitelist = ['https://www.yoursite.com', 'http://127.0.0.1:5500', 'http://localhost:3500'];
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (whitelist.indexOf(origin) !== -1 || !origin) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    optionsSuccessStatus: 200
+}
+app.use(cors(corsOptions));
+
+app.use(express.urlencoded({extended: false}));
+
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, '/public')));
+
 
 app.get('^/$|/index(.html)?', (req, res)=>{
     res.sendFile(path.join(__dirname, 'views', 'index.html'));
@@ -13,8 +38,15 @@ app.get('/new-page(.html)?', (req, res)=>{
 app.get('/old-page(.html)?', (req, res)=>{
     res.redirect(301, '/new-page.html');
 });
-app.get('/*', (req, res)=>{
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+app.all('*', (req, res) => {
+    res.status(404);
+    if (req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'views', '404.html'));
+    } else if (req.accepts('json')) {
+        res.json({ "error": "404 Not Found" });
+    } else {
+        res.type('txt').send("404 Not Found");
+    }
 });
 
 app.get('hello(.html)?', (req, res, next)=>{
@@ -23,5 +55,7 @@ app.get('hello(.html)?', (req, res, next)=>{
 },(req,res)=>{
     res.send('Hello World');
 });
+
+app.use(errorHandler)
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
